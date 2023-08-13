87bfe7bbf2113f3e676131dc9d41a33bcc2108c8
gabrielviggianih <gabrielviggiani0@gmail.com>
2023-08-12 22:04:44 -0300

    3 exercicios. aprendi sobre split

---
+++ exercicio_estrutura_de_dados.py
@@ -0,0 +1,26 @@
+# import time
+
+# altura = float(input("Digite a sua altura: "))
+# peso = float(input("digite o seu peso: "))
+
+# imc = peso / altura ** 2
+
+# print("Calculando...")
+# time.sleep(3)                      pra usar biblioteca importada time, 3 segundos pra responder
+# print(f"Seu imc é {imc:.2f}")   :2f pra pegar decimal dessa forma format
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# nome = input("Boa noite, qual o seu nome? ")
+# primeiro_nome = nome.split()[0]         divide o nome em lista pelos espaços e pega o index 0 com o colchete
+# print(f"Ola {primeiro_nome}.")
+
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+# email = input("digite aqui seu email: ")
+# dominio = email.split("@")[-1]                sepaga o email com o split no @ e com o colchete em -1 pega a ultima parte
+# print(dominio)
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------
