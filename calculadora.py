8e48e527152c183e46d2b4a2b0eaf388c25803aa
gabrielviggianih <gabrielviggiani0@gmail.com>
2023-08-14 18:27:13 -0300

    adicionado alguns exercicios e calculadora modelo1

---
+++ calculadora.py
@@ -0,0 +1,26 @@
+while True:
+    print("0 - Soma")
+    print("1 - Subtracao")
+    print("2 - Multiplicacao")
+    print("3 - Divisao")
+    print("4 - Exponenciacao")
+    operacao = input("Qual operacao voce deseja fazer? ")
+    n1 = float(input("Digite o primeiro numero: "))
+    n2 = float(input("Digite o segundo numero: "))
+    if operacao == "0":
+        print(f"A soma de {n1} e {n2} = {n1 + n2}")
+    elif operacao == "1":
+        print(f"A subracao de {n1} por {n2} = {n1 - n2}")
+    elif operacao == "2":
+        print(f"A multiplicacao de {n1} por {n2} = {n1 * n2}")
+    elif operacao == "3":
+        print(f"A divisao de {n1} por {n2} = {n1 / n2}")
+    elif operacao == "4":
+        print(f"A potencia de {n1} elevado a {n2} = {n1 ** n2}")
+    else:
+        print("Operacao nao encontrada.")
+    questao = input("Deseja continuar usando a calculadora? 0 para SIM, 1 para NAO: ")
+    if questao == "0":
+        continue
+    else:
+        break
---
+++ exercicio_operadores_comparacao_lacos.py
@@ -0,0 +1,48 @@
+# n1 = float(input("Digite a primeira nota: "))
+# n2 = float(input("Digite a segunda nota: "))
+
+# media = (n1 + n2) / 2
+
+# if media >= 7 and media < 10:
+#     print("Aprovado")
+# elif media <= 7:
+#     print("Reprovado")
+# elif media == 10:
+#     print("Aprovado com distinção.")
+    
+#------------------------------------------------------------------
+
+# list_nums = []
+# for i in range (3):
+#     input("Informe o {}° numero: ".format(i + 1))
+#     x = int(input())
+#     list_nums.append(x)
+
+# list_nums.sort()
+# print("O maior numero é: {}".format(list_nums[-1]))
+# print("O menor numero é: {}".format(list_nums[0]))
+
+#------------------------------------------------------------------
+
+# nome = input("Informe seu nome: ")
+# for i in range(len(nome)):
+#     print(nome[:i+1])
+    
+#------------------------------------------------------------------
+
+# print("Qual o tamanho da sequência de Fibonancci você deseja ver?")
+# n = int(input())
+
+# v0 = 0
+# v1 = 1
+# v = 1
+# print("========================")
+# print(1)
+# for i in range(n+1):
+#     print(v)
+#     v0 = v1
+#     v1 = v
+#     v = v1 + v0
+    
+#------------------------------------------------------------------
+
